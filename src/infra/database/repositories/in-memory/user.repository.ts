cb706b1dd5845b2fb11a728acdc5a6444a7a2f53
Filipe Siqueira <filipedev040990@gmail.com>
2025-04-24 17:52:03 -0300

    feat: add list super users repositorie method

--- src/infra/database/repositories/in-memory/user.repository.ts
+++ src/infra/database/repositories/in-memory/user.repository.ts
@@ -6,4 +6,14 @@ export default class UserRepository implements UserRepositoryInterface {
   async save(input: UserRepositoryData): Promise<void> {
     this.users.push(input)
   }
+
+  async listSuperUsers(): Promise<UserRepositoryData[] | null> {
+    const superUsers = this.users.filter((user) => user.score >= 900 && user.ativo)
+
+    if (!superUsers.length) {
+      return null
+    }
+
+    return superUsers
+  }
 }
